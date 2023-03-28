de135f3268c1b775de1a7b7507efb8cbf896b0d9
ivan.tsyhankov <ivan.tsyhankov@masterofcode.com>
2023-03-28 22:55:01 +0300

    add get deezer track by id

--- mzapi/jobs/save_track_stems.py
+++ mzapi/jobs/save_track_stems.py
@@ -8,7 +8,7 @@ from mzapi.jobs.models.job_state import JobStages
 
 
 @celery.task(bind=True, ignore_result=False)
-def save_track_stems(self, track_id=None, deezer_arl_token=None):
+def save_track_stems(self, track_id=None, deezer_arl_token=None, config=None):
     task_id = self.request.id
     DeezerDownloader.download(track_id=track_id, deezer_arl_token=deezer_arl_token, task_id=task_id)
     DeezerSpleeter.spleet(track_id, task_id=task_id)
--- mzapi/routes/tracks.py
+++ mzapi/routes/tracks.py
@@ -1,25 +1,25 @@
-import json
 from fastapi import APIRouter, Header
-from fastapi.responses import JSONResponse
 from .schemas import StemConfiguration
 from mzapi.jobs.save_track_stems import save_track_stems
 from mzapi.jobs.models.job_state import JobState
+import deezer
 
 router = APIRouter()
 
 
-@router.get("/track/favorites")
-async def favorites():
-    with open("mzapi/moc/track/favorites.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-        return JSONResponse(content=data)
-
-
-@router.post("/track/{track_id}/stem")
+@router.post("/tracks/{track_id}/stem")
 async def create_item(track_id: int, config: StemConfiguration,
                       deezer_arl_token: str = Header(None, alias="X-DEEZER-ARL-TOKEN")):
     if deezer_arl_token is None:
         return {"message": "Missing Deezer ARL Token header"}
 
-    result = save_track_stems.delay(track_id=track_id, deezer_arl_token=deezer_arl_token)
+    result = save_track_stems.delay(track_id=track_id, deezer_arl_token=deezer_arl_token, config=config)
     return JobState(job_id=result.id, progress=0, stage=0)
+
+
+@router.get('/tracks/{track_id}')
+async def get_track_by_id(track_id: int, deezer_arl_token: str = Header(None, alias="X-DEEZER-ARL-TOKEN")):
+    dz = deezer.Deezer()
+    dz.login_via_arl(arl=deezer_arl_token)
+    track = dz.api.get_track(track_id)
+    return track
